00a4b10cbc9620456bb1b6e7f6dc97e1655a7889
tangcheng <tangcheng_cd@csii.com.cn>
2021-03-05 23:49:36 +0800

    aria2 config

--- script/startaria2.sh
+++ script/startaria2.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# init .ara2 config file
+if [ ! -d $HOME/.aria2 ]; then
+  mkdir $HOME/.aria2
+fi
+
+if [ ! -f $HOME/.aria2/aria2.session ]; then
+  touch $HOME/.aria2/aria2.session
+fi
+
+if [ ! `command -v aria2c` ]; then
+  echo -e "\033[31m aria2c command not foud! Please install Arial2c. \033[0m"
+  exit
+fi
+
+# start aria2c
 aria2c -D --conf-path=./.aria2/.aria2.conf
 
 
