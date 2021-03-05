b46a6f6a8f8ea8a9cf7bb7b7f52c2b6691274a63
tangcheng <tangcheng_cd@csii.com.cn>
2021-03-05 23:51:52 +0800

    aria2 config

--- script/killAria2.sh
+++ script/killAria2.sh
@@ -1,3 +1,9 @@
 #！/bin/bash
 
+if [ ! `command -v aria2c` ]; then
+  echo -e "\033[31m aria2c command not foud! Please install Arial2c. \033[0m"
+  exit
+fi
+
+
 ps -ef | grep aria2c | awk '{print $2}' | head -n 1 | xargs kill -9
