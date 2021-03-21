f083efbbde87dfe6a15c7b126dab69c45e151a40
tangcheng <tangcheng_cd@csii.com.cn>
2021-03-21 11:38:41 +0800

    script optimization

--- script/githubDocStart.sh
+++ script/githubDocStart.sh
@@ -7,12 +7,20 @@ document_path='/media/tc/jx-file/2_STUDY/GitHubRepositories/00myGitHubRepository
 
 # program
 pwd=$(pwd)
-existsPort=$(ps -ef | awk 'BEGIN{pre="http.server '$server_port'$"}{if($0 ~ pre) print $2}' )
-if [ -z $ex ]; then
-    cd $document_path
-    nohup python -m http.server $server_port >/dev/null 2>&1 &
-    cd $pwd
+existsPID=$(ps -ef | awk 'BEGIN{pre="http.server '$server_port'$"}{if($0 ~ pre) print $2}' )
+
+if [[ -n $existsPID ]]; then
+  echo Detected that the document service has started, PID:$existsPID
+  echo Restart in progress.
+
+  kill -9 $existsPID
 fi
+
+echo start document service....
+cd $document_path
+nohup python -m http.server $server_port >/dev/null 2>&1 &
+cd $pwd
+echo document service started.
 
 # electron-webbox今后做一个脚本钩子就好了，可以实现启动前运行脚本，退出前运行脚本
 electron-webbox --args url="$server_page_url" maximize
