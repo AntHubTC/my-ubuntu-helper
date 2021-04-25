c72e5dce5f1d265ec0191aa9bd635c4674a635bb
tangcheng <tangcheng_cd@csii.com.cn>
2021-04-25 23:58:38 +0800

    change icon aria2 conf sh mss

--- script/mutiScreenSetting.sh
+++ script/mutiScreenSetting.sh
@@ -6,8 +6,9 @@ echo        2. 只使用VGA
 echo        3. 只使用DP
 echo        4. 使用笔记本屏幕和VGA
 echo        5. 使用笔记本屏幕和DP
-echo        6. 使用VGA和DP
-echo        7. 查看当前配置
+echo        6. 使用VGA和DP[扩展]
+echo        7. 使用VGA和DP[复制]
+echo        8. 查看当前配置
 echo 注意: 有一些命令不是很稳定，应该是图形界面自动对屏幕的调节对这个程序命令的冲突。但有时候自动调节没有达到我们预期的效果，这个程序就可以弥补一些问题。
 echo ================================================================
 read -p '请输入你的选择：' -n 1 opt
@@ -38,5 +39,9 @@ elif [ $opt -eq 6 ]; then
          --output VGA-1-1 --auto --rotate normal --mode '1920x1080' --primary \
          --output DP-1-1 --auto --rotate right --pos 0x0 --mode '2560x1440' --left-of VGA-1-1
 elif [ $opt -eq 7 ]; then
+  xrandr --output eDP-1-1 --off \
+         --output DP-1-1 --auto --rotate right --pos 0x0 --mode '2560x1440' \
+         --output VGA-1-1 --auto --rotate right --mode '1920x1080' --primary --same-as DP-1-1
+elif [ $opt -eq 8 ]; then
   xrandr -q
 fi
